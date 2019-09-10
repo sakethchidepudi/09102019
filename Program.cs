631199935e9117534be4b8e298003cd40e8c9279
sakethchidepudi <chidepudis@mail.usf.edu>
2019-09-10 16:43:22 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Conditional_Statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("How many years of experience do you have in professional programming?");
+            
+        
+            
+                
+                int input = int.Parse(Console.ReadLine());
+
+
+                if (input == 0)
+                {
+                    Console.WriteLine("Needslot of practice");
+                }
+                else if (input == 1)
+                {
+                    Console.WriteLine("looks like you have a lot of experience");
+                }
+                else if (input == 2)
+                {
+                    Console.WriteLine("Wow! you have been doing this for a little while");
+                }
+                else
+                {
+                    Console.WriteLine("expert");
+                }
+                           
+                
+               
+        } // end of Main
+    } // end of class
+} // end of namespace
